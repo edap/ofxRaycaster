369b9e11fa7068d4e8e818decdae990310353eec
Davide Prati <lastexxit@@gmail.com>
2019-03-19 17:58:22 +0100

    indentation

--- example-3d/src/ofApp.h
+++ example-3d/src/ofApp.h
@@ -4,37 +4,36 @@
 #include "ofxRaycaster.h"
 
 class ofApp : public ofBaseApp{
-
 	public:
-		void setup();
-		void update();
-		void draw();
+        void setup();
+        void update();
+        void draw();
 
-		void keyPressed(int key);
+        void keyPressed(int key);
 
-    void drawSphereIntersection();
-    void drawPlaneIntersection();
-    void drawTriangleIntersection();
-    void drawPrimitiveIntersection();
-    void drawLine(glm::vec3 a, glm::vec3 b, ofColor color);
+        void drawSphereIntersection();
+        void drawPlaneIntersection();
+        void drawTriangleIntersection();
+        void drawPrimitiveIntersection();
+        void drawLine(glm::vec3 a, glm::vec3 b, ofColor color);
 
-    ofEasyCam camera;
-    ofLight light;
-    ofMaterial material;
-    int scene = 4;
-    glm::vec3 lookAtRay = glm::vec3(0, 200, 0);
+        ofEasyCam camera;
+        ofLight light;
+        ofMaterial material;
+        int scene = 4;
+        glm::vec3 lookAtRay = glm::vec3(0, 200, 0);
 
-    ofNode center;
-    float sphereRadius = 200;
-    ofSpherePrimitive sphere;
-    ofBoxPrimitive box;
-    ofPlanePrimitive planePrimitive;
-    glm::vec3 planeOrigin = glm::vec3(0,500,0);
-    glm::vec3 planeNormal = glm::vec3(0,-1,0);
-    ofNode triangleLookAt;
+        ofNode center;
+        float sphereRadius = 200;
+        ofSpherePrimitive sphere;
+        ofBoxPrimitive box;
+        ofPlanePrimitive planePrimitive;
+        glm::vec3 planeOrigin = glm::vec3(0,500,0);
+        glm::vec3 planeNormal = glm::vec3(0,-1,0);
+        ofNode triangleLookAt;
 
-    glm::vec3 rayDirection;
-    glm::vec3 rayOrigin;
-    ofxraycaster::Ray ray;
-    ofxraycaster::Plane plane;
+        glm::vec3 rayDirection;
+        glm::vec3 rayOrigin;
+        ofxraycaster::Ray ray;
+        ofxraycaster::Plane plane;
 };
