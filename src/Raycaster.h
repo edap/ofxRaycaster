2b411e551a124588428e96a3f57da27c278eaedf
Davide Prati <lastexxit@@gmail.com>
2019-03-29 15:36:20 +0100

    WIP mouse picker

---
+++ src/Raycaster.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "ofMain.h"
+#include "Ray.h"
+
+namespace ofxraycaster {
+    class Raycaster {
+    public:
+        Raycaster(){};
+        Raycaster(const glm::vec3 _origin, const glm::vec3 _direction, const float near, const float far);
+        void set(const glm::vec3 _origin, const glm::vec3 _direction);
+        void setFromCamera(const glm::vec2 coords, const ofCamera camera);
+        void draw();
+
+    private:
+        Ray ray;
+        float near;
+        float far;
+    };
+}
